2597684a8d22d2a936cab4f76af1dfc522042c5d
Adi Gabber <adi@tunityapp.com>
2015-06-23 12:57:49 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+var http = require('http')
+var request = require('request')
+var fs = require('fs')
+var _ = require('underscore')
+
+var main = function(opts) {
+  var url = opts.url
+  var port = opts.port
+
+  var routes = _.map(opts.routes, function(route) {
+    return {
+      'channel': route.channel,
+      'regex': eval(route.regex)
+    }
+  })
+
+  console.info(routes)
+
+
+  var server = http.createServer(function(req, res) {
+
+    var body = ''
+
+    req.on('data', function(chunk) {
+      body += chunk
+    })
+
+    req.on('end', function() {
+      var json = JSON.parse(body)
+
+      console.log(routes ,'routes')
+
+      var r = _.find(routes, function(r) {
+        return r.regex.test(body)
+      })
+
+      if(r) {
+        json.channel = r.channel
+      }
+
+      request.post( { url:url , json:true , body:json}).pipe(res)
+
+    })
+  });
+
+  server.listen(port);
+  console.log("Server is listening on port " + port);
+
+}
+
+if (require.main === module) {
+  // read command line
+  var opts
+  var argv = require('minimist')(process.argv.slice(2))
+  if (argv.config) {
+    opts = JSON.parse(fs.readFileSync(argv.config))
+  }
+
+  main(opts)
+}
